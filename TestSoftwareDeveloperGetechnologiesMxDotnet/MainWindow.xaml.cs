fcac7678db9d3304a6c271686e29c92a54510fe6
enriqueh2342 <enriqueh2342@hotmail.com>
2025-05-10 13:18:35 -0600

    Se agrega formulario a front

--- TestSoftwareDeveloperGetechnologiesMxDotnet/MainWindow.xaml.cs
+++ TestSoftwareDeveloperGetechnologiesMxDotnet/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using TestSoftwareDeveloperGetechnologiesMxDotnet.Models;
+using TestSoftwareDeveloperGetechnologiesMxDotnet.Tools;
 
 namespace TestSoftwareDeveloperGetechnologiesMxDotnet
 {
@@ -20,13 +21,23 @@ namespace TestSoftwareDeveloperGetechnologiesMxDotnet
     public partial class MainWindow : Window
     {
         private static readonly HttpClient _httpClient = new HttpClient();
+        private Validations validations = new Validations();
         public MainWindow()
         {
             InitializeComponent();
+            SizeChanged += MainWindow_SizeChanged;
+            UpdateLayoutBasedOnWidth(ActualWidth);
         }
 
-        private async void Send_Click(object sender, RoutedEventArgs e)
+        private async void Button_Send(object sender, RoutedEventArgs e)
         {
+            Person personForm = await GetPersonFromForm();
+            var validateModel = validations.ValidateModel(personForm);
+            if (!validateModel.isvalid)
+            {
+                MessageBox.Show(validateModel.message);
+            }
+
             List<WeatherForecast> weatherForecastsList = await GetAsync(ApiEndpoints.WeatherForecastEndpoint);
         }
 
@@ -55,5 +66,69 @@ namespace TestSoftwareDeveloperGetechnologiesMxDotnet
         {
 
         }
+
+        private void MainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            UpdateLayoutBasedOnWidth(e.NewSize.Width);
+        }
+
+        private void UpdateLayoutBasedOnWidth(double width)
+        {
+            if (width < 600)
+            {
+                Grid.SetRow(LogoImage, 0);
+                Grid.SetColumn(LogoImage, 0);
+                Grid.SetColumnSpan(LogoImage, 2);
+
+                Grid.SetRow(FormPanel, 1);
+                Grid.SetColumn(FormPanel, 0);
+                Grid.SetColumnSpan(FormPanel, 2);
+
+                LogoImage.HorizontalAlignment = HorizontalAlignment.Center;
+                LogoImage.VerticalAlignment = VerticalAlignment.Center;
+                LogoImage.Margin = new Thickness(0, 10, 0, 300); 
+
+
+                FormPanel.HorizontalAlignment = HorizontalAlignment.Center;
+                FormPanel.VerticalAlignment = VerticalAlignment.Center;
+                FormPanel.Margin = new Thickness(0, 0, 0, 10);
+            }
+            else
+            {
+                Grid.SetRow(LogoImage, 0);
+                Grid.SetColumn(LogoImage, 0);
+                Grid.SetRowSpan(LogoImage, 2);
+                Grid.SetColumnSpan(LogoImage, 1);
+
+                Grid.SetRow(FormPanel, 0);
+                Grid.SetColumn(FormPanel, 1);
+                Grid.SetRowSpan(FormPanel, 2);
+                Grid.SetColumnSpan(FormPanel, 1);
+
+                LogoImage.HorizontalAlignment = HorizontalAlignment.Center;
+                LogoImage.VerticalAlignment = VerticalAlignment.Center;
+                LogoImage.Margin = new Thickness(40, 20, 40, 20);
+
+                FormPanel.HorizontalAlignment = HorizontalAlignment.Center;
+                FormPanel.VerticalAlignment = VerticalAlignment.Center;
+                FormPanel.Margin = new Thickness(40, 20, 40, 20);
+            }
+        }
+
+        private async Task<Person> GetPersonFromForm()
+        {
+            Person person = new Person() {
+                Name = Name.Text,
+                MiddleName = MiddleName.Text,
+                LastName = LastName.Text,
+                Identification = Identification.Text
+            };
+
+            return person;
+        }
+
+
+
+
     }
 }
---
+++ TestSoftwareDeveloperGetechnologiesMxDotnet/Tools/Validations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestSoftwareDeveloperGetechnologiesMxDotnet.Models;
+
+namespace TestSoftwareDeveloperGetechnologiesMxDotnet.Tools
+{
+    public class Validations
+    {
+        public (bool isvalid, string message) ValidateModel(Person person)
+        {
+            if (string.IsNullOrEmpty(person.Name))
+                return (false, "Ingrese el nombre");
+
+            if (string.IsNullOrEmpty(person.MiddleName))
+                return (false, "Ingrese el Apellido Paterno");
+
+            if (string.IsNullOrEmpty(person.Identification))
+                return (false, "Ingrese su Curp");
+
+            return (true, string.Empty);
+        }
+    }
+}
