9572f1ce9323f0e5678752e916c07871106d59b0
enriq <enriq@PC-LUIS>
2025-05-10 10:45:55 -0600

    Agregar archivos de proyecto.

---
+++ TestSoftwareDeveloperGetechnologiesMxDotnet/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TestSoftwareDeveloperGetechnologiesMxDotnet.Models;
+
+namespace TestSoftwareDeveloperGetechnologiesMxDotnet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly HttpClient _httpClient = new HttpClient();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Send_Click(object sender, RoutedEventArgs e)
+        {
+            List<WeatherForecast> weatherForecastsList = await GetAsync(ApiEndpoints.WeatherForecastEndpoint);
+        }
+
+        public async Task<List<WeatherForecast>> GetAsync(string endpoint)
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync($"{ApiEndpoints.BaseUrl}/{endpoint}");
+                response.EnsureSuccessStatusCode();
+                string jsonString = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<List<WeatherForecast>>(jsonString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Error de solicitud HTTP: {ex.Message}");
+                return default;
+            }
+            catch (JsonException ex)
+            {
+                Console.WriteLine($"Error al deserializar JSON: {ex.Message}");
+                return default;
+            }
+        }
+
+        private void Name_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ TestSoftwareDeveloperGetechnologiesMxDotnet/Models/ApiEndpoints.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSoftwareDeveloperGetechnologiesMxDotnet.Models
+{
+    public class ApiEndpoints
+    {
+        public const string BaseUrl = "https://localhost:7083";
+        public const string WeatherForecastEndpoint = "WeatherForecast";
+    }
+}
